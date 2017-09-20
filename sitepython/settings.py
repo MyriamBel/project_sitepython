0bb405777854b4fa9562b3fc82cbaab26d26801c
MyriamBel <nikitokmalishok@mail.ru>
2017-09-20 17:40:17 +0300

    fix auth method postgres user, create first page and edit url,views for this

--- sitepython/settings.py
+++ sitepython/settings.py
@@ -122,5 +122,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'templates/sitepython3/static/'
+STATIC_URL = os.path.join(BASE_DIR, '/static/')
 STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, '/assets/static'),
+        os.path.join(BASE_DIR,'templates/sitepython3/static'),
+        )
+
+MEDIA_URL = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media')
